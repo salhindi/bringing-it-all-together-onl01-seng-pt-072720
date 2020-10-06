200005cd002c2aa5382c61e2d7864b7217082550
Sara A <calisaraalhindi@gmail.com>
2020-10-06 16:10:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,4 +73,14 @@ class Dog
     new_dog
   end
   
+  def self.find_by_name(name)
+    sql <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
+  end
+  
 end
