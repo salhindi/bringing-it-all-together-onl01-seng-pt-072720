74ca120f595f4165341cdf4edf63a9492d3e7bd2
Sara A <calisaraalhindi@gmail.com>
2020-10-06 02:55:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,49 @@
+class Dog 
+  attr_accessor :name, :breed
+  attr_reader :id
+  
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        breed TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+      SQL
+      
+    DB[:conn].execute(sql)
+  end
+
+  def save
+  sql = <<-SQL
+    INSERT INTO dogs (name, breed) 
+    VALUES (?,?)
+    SQL
+    
+  DB[:conn].execute(sql, self.name, self.breed)
+    
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  self
+  end
+  
+  def self.create(name:, breed:)
+    dog = Dog.new(name, breed)
+    dog.save
+    dog
+  end
+    
+  
+end
